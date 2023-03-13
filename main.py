41231266239eaea4035a22dec4729c9f09574e42
bobitro32 <102661584+bobitro32@users.noreply.github.com>
2023-03-13 18:36:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import time
 import HadnTrackingModule as htm
 import numpy as np
+import serial
 
 import math
 
@@ -11,8 +12,9 @@ import math
 
 cap = cv.VideoCapture(0)
 detector = htm.HandDetector(minDetectionConf=0.7,maxHands=2)
-commands = ["Stay","Up","Down","Right","Left","Forward","Come back"]
+commands = ["Stay","Forward","Back","Right","Left"]
 index=0
+#wifi =serial.Serial('/dev/ttyUSB0',115200)
 while True:
     success, img = cap.read()
     img =cv.flip(img,2)
@@ -22,8 +24,11 @@ while True:
         # Hand 1
         hand1 = hands[0]
         fingers1 = detector.fingersUp(hand1)
+        count = fingers1.count(1)
+        print(count)
         bbox = hand1["bbox"]
-
+        #string = 'X{0}'.format(count)#send the data of how many fingers we have
+        #wifi.write(string.encode("utf-8"))
         handType1 = hand1["type"]
         if(handType1 =="Right"):
             if (fingers1 == [0,1,1,1,1]):
@@ -40,46 +45,32 @@ while True:
                 index =1
                 cv.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
                              (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),(106, 0, 255), 2)
-                #up
+                #go forward
                 cv.putText(img, commands[index], (bbox[0] - 30, bbox[1] - 30), cv.FONT_HERSHEY_PLAIN,
                            2, (0, 0, 0), 2)
-            elif (fingers1 ==[1,1,1,0,0,]):
-                #down
+            elif (fingers1 ==[0,1,1,0,0,]):
+                #back
                 index = 2
                 cv.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
                              (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20), (244, 114, 203), 2)
                 cv.putText(img, commands[index], (bbox[0] - 30, bbox[1] - 30), cv.FONT_HERSHEY_PLAIN,
                            2, (0, 0, 0), 2)
 
-            elif (fingers1 == [1,0,0,0,1]):
+            elif (fingers1 == [1,1,1,1,1]):
                 #right
                 index = 3
                 cv.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
                              (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20), (130, 90, 44), 2)
                 cv.putText(img, commands[index], (bbox[0] - 30, bbox[1] - 30), cv.FONT_HERSHEY_PLAIN,
                            2, (0, 0, 0), 2)
-            elif (fingers1 == [0,0,0,0,0]):
+            elif (fingers1 == [1,1,1,0,0]):
                 #left
                 index =4
                 cv.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
                              (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20), (188,143,143), 2)
                 cv.putText(img, commands[index], (bbox[0] - 30, bbox[1] - 30), cv.FONT_HERSHEY_PLAIN,
                            2, (0, 0, 0), 2)
-            elif(fingers1 ==[0,1,1,0,0,]):
-                #forward
-                index = 5
-                cv.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-                             (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20), (0, 250, 154), 2)
-                cv.putText(img, commands[index], (bbox[0] - 30, bbox[1] - 30), cv.FONT_HERSHEY_PLAIN,
-                           2, (0, 0, 0), 2)
-            elif(fingers1 == [0,1,0,0,0]):
-                #Come back
-                index = 6
-                cv.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-                             (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20), (255,255,255), 2)
-                cv.putText(img, commands[index], (bbox[0] - 30, bbox[1] - 30), cv.FONT_HERSHEY_PLAIN,
-                           2, (0, 0, 0), 2)
-
+            
 
 
 
